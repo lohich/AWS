56c114e79d5839971d860264ec575ff318da9228
Alexey Sych <Alexey_Sych@epam.com>
2020-02-04 14:21:28 +0300

    init commit

---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Controller]
+    [Route("Books")]
+    public class BooksController : Controller
+    {
+        [HttpGet("{isbn}")]
+        public Book Get(string isbn)
+        {
+            return new Book();
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Enumerable.Empty<Book>();
+        }
+
+        [HttpDelete("{isbn}")]
+        public void Delete(string isbn)
+        {
+            
+        }
+
+        [HttpPost]
+        public Book Post([FromBody]Book item)
+        {
+            return new Book();
+        }
+
+        [HttpPut("{isbn}")]
+        public Book Get(string isbn, [FromBody]Book item)
+        {
+            return new Book();
+        }
+    }
+}
