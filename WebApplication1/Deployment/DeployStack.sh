ba5a8a168e9063cfbf552b34600b16b76948a8a4
Alexey Sych <Alexey_Sych@epam.com>
2020-02-13 13:19:28 +0300

    .

--- WebApplication1/Deployment/DeployStack.sh
+++ WebApplication1/Deployment/DeployStack.sh
@@ -1,5 +1,5 @@
 aws cloudformation deploy --template-file ./WebApplication1/Deployment/cloudformation.yml --stack-name Second
-if [$? -eq 0] || [$? -eq 255]; then
+if [ $? -eq 0 ] || [ $? -eq 255 ]; then
     exit 0
 else
     exit 1
