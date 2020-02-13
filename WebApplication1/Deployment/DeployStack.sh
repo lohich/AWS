6960ba719fedaf06d513a36ca173bb665b20124a
Alexey Sych <Alexey_Sych@epam.com>
2020-02-13 13:30:47 +0300

    last test

--- WebApplication1/Deployment/DeployStack.sh
+++ WebApplication1/Deployment/DeployStack.sh
@@ -1,4 +1,4 @@
-aws cloudformation deploy --template-file ./WebApplication1/Deployment/cloudformation.yml --stack-name Second
+aws cloudformation deploy --template-file ./WebApplication1/Deployment/cloudformation.yml --stack-name Books
 echo $?
 if [ $? -eq 0 ] || [ $? -eq 255 ]; then
     exit 0
