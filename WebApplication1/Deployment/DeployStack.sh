7d48b8293e5e6e2ff984b06d8055fc66e3f8fddd
Alexey Sych <Alexey_Sych@epam.com>
2020-02-13 17:42:50 +0300

    no fail on empty change stack

--- WebApplication1/Deployment/DeployStack.sh
+++
@@ -1,7 +0,0 @@
-aws cloudformation deploy --template-file ./WebApplication1/Deployment/cloudformation.yml --stack-name Books --parameter-overrides EC2AccessKey=Admin --capabilities CAPABILITY_IAM
-echo $?
-if [ $? -eq 0 ] || [ $? -eq 255 ]; then
-    exit 0
-else
-    exit $?
-fi
