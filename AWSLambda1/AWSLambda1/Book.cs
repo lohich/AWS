f941496331b4285949fe1b99071f9347b4931465
Alexey Sych <Alexey_Sych@epam.com>
2020-02-14 16:52:00 +0300

    .

--- AWSLambda1/AWSLambda1/Book.cs
+++ AWSLambda1/AWSLambda1/Book.cs
@@ -1,11 +1,7 @@
-﻿using Amazon.DynamoDBv2.DataModel;
-
-namespace WebApplication1.Models
+﻿namespace WebApplication1.Models
 {
-    [DynamoDBTable("books")]
     public class Book
     {
-        [DynamoDBHashKey]
         public string ISBN { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
